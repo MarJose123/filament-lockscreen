0fe8610d9e9c6795877c120d47021d7b079bc3b1
MarJose <18107626+MarJose123@users.noreply.github.com>
2023-08-22 11:48:40 +0800

    fixed routing issue

--- src/Lockscreen.php
+++ src/Lockscreen.php
@@ -6,10 +6,11 @@ use Filament\Contracts\Plugin;
 use Filament\Navigation\MenuItem;
 use Filament\Navigation\UserMenuItem;
 use Filament\Panel;
+use Filament\PanelProvider;
 use Livewire\Livewire;
 use lockscreen\FilamentLockscreen\Http\Livewire\LockerScreen;
 
-class Lockscreen  implements Plugin
+class Lockscreen implements Plugin
 {
     protected bool $hasEnabledLockscreen = false;
 
@@ -30,20 +31,20 @@ class Lockscreen  implements Plugin
 
     public function register(Panel $panel): void
     {
-        $panel
-            ->userMenuItems([
-                MenuItem::make()
-                    ->label(__('filament-lockscreen::default.user_menu_title'))
-                    ->url(route("lockscreen.{$panel->getId()}.page"))
-                    ->icon(config('filament-lockscreen.icon')),
-            ]);
+
     }
 
     public function boot(Panel $panel): void
     {
+
+        $panel->userMenuItems([
+            'lockscreen' => MenuItem::make()
+                ->label(__('filament-lockscreen::default.user_menu_title'))
+                ->url(route("lockscreen.{$panel->getId()}.page"))
+                ->icon(config('filament-lockscreen.icon')),
+        ]);
+
         Livewire::component('LockerScreen', LockerScreen::class);
-
     }
-
 
 }
