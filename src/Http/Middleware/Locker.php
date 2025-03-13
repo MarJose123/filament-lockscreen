cce0b9c45543b7bce85f282c762ed44d83586c2d
MarJose123 <18107626+MarJose123@users.noreply.github.com>
2025-03-13 18:56:10 +0800

    ♻️ refactor(`Locker.php`): Improve `Locker` middleware to handle Livewire requests and enhance path matching logic for preventing blocking other non-panel path request.

--- src/Http/Middleware/Locker.php
+++ src/Http/Middleware/Locker.php
@@ -3,24 +3,50 @@
 namespace lockscreen\FilamentLockscreen\Http\Middleware;
 
 use Closure;
-use Psr\Container\ContainerExceptionInterface;
-use Psr\Container\NotFoundExceptionInterface;
+use Exception;
+use Illuminate\Http\RedirectResponse;
+use Illuminate\Http\Request;
+use Illuminate\Support\Str;
+use Livewire\LivewireManager;
 
 class Locker
 {
     /**
-     * @throws ContainerExceptionInterface
-     * @throws NotFoundExceptionInterface
-     * @throws \Exception
+     * @param Request $request
+     * @param Closure $next
+     * @return RedirectResponse|mixed
+     *
+     * @throws Exception
      */
-    public function handle($request, Closure $next)
+    public function handle(Request $request, Closure $next): mixed
     {
-        if ($request->method() === 'GET' && $request->session()->get('lockscreen')) {
+        if ($this->isLivewireRequest()) {
+            return $next($request);
+        }
+
+        if ($request->isMethod('GET') && $request->session()->get('lockscreen') && $this->isSegmentMatched($request)) {
             $panelId = filament()->getCurrentPanel()?->getId();
 
             return redirect()->route("lockscreen.{$panelId}.page");
         }
 
         return $next($request);
+    }
+
+    /**
+     * @param $request
+     * @return boolean
+     */
+    protected function isSegmentMatched($request): bool
+    {
+        $panelPath = filament()->getCurrentPanel()?->getPath();
+        $guardLockPath = Str::of($panelPath)->remove('/');
+
+        return $request->is([$guardLockPath, $guardLockPath->append('/*')]);
+    }
+
+    protected function isLivewireRequest(): bool
+    {
+        return class_exists(LivewireManager::class) && app(LivewireManager::class)->isLivewireRequest();
     }
 }
