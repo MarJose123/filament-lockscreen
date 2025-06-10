e504d13ec0ee55eac6baf93c7fa5530f0cf69a88
MarJose123 <18107626+MarJose123@users.noreply.github.com>
2025-06-10 18:58:08 +0800

    ✨ feat(Lockscreen): Add `HasLockscreenConfiguration` trait and dynamic icon support

--- src/Lockscreen.php
+++ src/Lockscreen.php
@@ -5,12 +5,15 @@ namespace lockscreen\FilamentLockscreen;
 use Filament\Contracts\Plugin;
 use Filament\Navigation\MenuItem;
 use Filament\Panel;
+use Filament\Support\Concerns\EvaluatesClosures;
 use Livewire\Livewire;
+use lockscreen\FilamentLockscreen\Concerns\HasLockscreenConfiguration;
 use lockscreen\FilamentLockscreen\Http\Livewire\LockerScreen;
 
 class Lockscreen implements Plugin
 {
-    protected bool $hasEnabledLockscreen = false;
+    use EvaluatesClosures;
+    use HasLockscreenConfiguration;
 
     public function getId(): string
     {
@@ -36,7 +39,7 @@ class Lockscreen implements Plugin
             'lockscreen' => MenuItem::make()
                 ->label(fn () => __('filament-lockscreen::default.user_menu_title'))
                 ->postAction(route("lockscreen.{$panel->getId()}.lock-session"))
-                ->icon(config('filament-lockscreen.icon')),
+                ->icon($this->getIcon()),
         ]);
 
         Livewire::component('LockerScreen', LockerScreen::class);
