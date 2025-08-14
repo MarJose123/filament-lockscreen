23219326880fc95e1784bd4bdd844a371a371240
MarJose123 <18107626+MarJose123@users.noreply.github.com>
2025-08-14 19:19:50 +0800

    refactor: Update URL retrieval and use helpers

--- routes/web.php
+++ routes/web.php
@@ -4,24 +4,25 @@ use Filament\Facades\Filament;
 use Illuminate\Support\Facades\Route;
 use lockscreen\FilamentLockscreen\Http\Livewire\LockerScreen;
 use lockscreen\FilamentLockscreen\Http\LockscreenSessionController;
+use lockscreen\FilamentLockscreen\Lockscreen;
 
 Route::name('lockscreen.')
-    ->group(function () {
+    ->group(function (): void {
         foreach (Filament::getPanels() as $panel) {
             $panelId = $panel->getId();
             $domains = $panel->getDomains();
 
-            foreach ((empty($domains) ? [null] : $domains) as $domain) {
+            foreach ((blank($domains) ? [null] : $domains) as $domain) {
                 Route::domain($domain)
                     ->middleware($panel->getMiddleware())
                     ->name("{$panelId}.")
                     ->prefix($panel->getPath())
-                    ->group(function () {
+                    ->group(function (): void {
                         Route::post('lock-session', [LockscreenSessionController::class, 'lockSession'])
                             ->name('lock-session');
                         Route::get(
-                            (config()->has('filament-lockscreen.url') && config('filament-lockscreen.url') !== '' && config('filament-lockscreen.url') !== '/')
-                                ? config('filament-lockscreen.url')
+                            (filled(Lockscreen::get()->getUrl()) && Lockscreen::get()->getUrl() !== '/')
+                                ? Lockscreen::get()->getUrl()
                                 : '/screen/lock',
                             LockerScreen::class
                         )->name('page');
