bada33ff32ee6cc3676ecbde4a25bb9806fac568
MarJose123 <18107626+MarJose123@users.noreply.github.com>
2025-08-15 18:27:21 +0800

    feat: Conditionally register lockscreen routes

--- routes/web.php
+++ routes/web.php
@@ -6,29 +6,34 @@ use lockscreen\FilamentLockscreen\Http\Livewire\LockerScreen;
 use lockscreen\FilamentLockscreen\Http\LockscreenSessionController;
 use lockscreen\FilamentLockscreen\Lockscreen;
 
-Route::name('lockscreen.')
-    ->group(function (): void {
-        foreach (Filament::getPanels() as $panel) {
-            $panelId = $panel->getId();
-            $domains = $panel->getDomains();
+if (Lockscreen::get()->isPluginEnabled()) {
+    Route::name('lockscreen.')
+        ->group(function (): void {
+            foreach (Filament::getPanels() as $panel) {
+                $panelId = $panel->getId();
+                $domains = $panel->getDomains();
 
-            foreach ((blank($domains) ? [null] : $domains) as $domain) {
-                Route::domain($domain)
-                    ->middleware($panel->getMiddleware())
-                    ->name("{$panelId}.")
-                    ->prefix($panel->getPath())
-                    ->group(function (): void {
-                        Route::post('lock-session', [LockscreenSessionController::class, 'lockSession'])
-                            ->name('lock-session');
-                        Route::get(
-                            (filled(Lockscreen::get()->getUrl()) && Lockscreen::get()->getUrl() !== '/')
-                                ? Lockscreen::get()->getUrl()
-                                : '/screen/lock',
-                            LockerScreen::class
-                        )->name('page');
-                    });
+                foreach ((blank($domains) ? [null] : $domains) as $domain) {
+                    Route::domain($domain)
+                        ->middleware(...$panel->getMiddleware())
+                        ->name("{$panelId}.")
+                        ->prefix($panel->getPath())
+                        ->group(function () use ($panel): void {
+                            /** @var \Filament\Panel $panel */
+                            if ($panel->hasPlugin(Lockscreen::get()->getId())) {
+                                Route::post('lock-session', [LockscreenSessionController::class, 'lockSession'])
+                                    ->name('lock-session');
+                                Route::get(
+                                    (filled(Lockscreen::get()->getUrl()) && Lockscreen::get()->getUrl() !== '/')
+                                        ? Lockscreen::get()->getUrl()
+                                        : '/screen/lock',
+                                    LockerScreen::class
+                                )->name('page');
+                            }
+                        });
+
+                }
 
             }
-
-        }
-    });
+        });
+}
