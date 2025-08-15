fc975dcfcba6344dbf9e6115dc6f70b115848a19
MarJose123 <18107626+MarJose123@users.noreply.github.com>
2025-08-15 18:25:48 +0800

    feat: Add HasSwitch trait for plugin state

---
+++ src/Traits/HasSwitch.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace lockscreen\FilamentLockscreen\Traits;
+
+use Filament\Support\Concerns\EvaluatesClosures;
+
+trait HasSwitch
+{
+    use EvaluatesClosures;
+
+    protected bool $enablePlugin = false;
+
+    /**
+     *  Enable the plugin
+     *
+     * @return $this
+     */
+    public function enablePlugin(\Closure|bool $enable = true): static
+    {
+        $enable = $this->evaluate($enable);
+        $this->enablePlugin = $enable;
+
+        return $this;
+    }
+
+    /**
+     * Check if the plugin is enabled
+     */
+    public function isPluginEnabled(): bool
+    {
+        return $this->enablePlugin;
+    }
+}
