ccfba5114281426767333a079d62cb027165016d
MarJose123 <18107626+MarJose123@users.noreply.github.com>
2025-08-16 23:30:13 +0800

    refactor: Simplify login blade template

--- resources/views/page/auth/login.blade.php
+++ resources/views/page/auth/login.blade.php
@@ -1,43 +1,17 @@
 <div>
     <x-filament-panels::page.simple>
         @props([
-            'after' => null,
             'heading' => null,
             'subheading' => null,
         ])
 
-        <main>
-            {{--Slot --}}
-            <div class="flex flex-row justify-center">
-                <img class="w-56 h-56 rounded-full" src="{{ \Filament\Facades\Filament::getUserAvatarUrl(\Filament\Facades\Filament::auth()->user())}}" alt="avatar">
-            </div>
-            <div class="flex flex-row justify-center">
-                <div class="font-medium dark:text-white">
-                    <div>{{\Filament\Facades\Filament::auth()->user()?->name ?? ''}}</div>
-                </div>
-            </div>
+        {{ $this->content }}
 
-            <x-filament-panels::form wire:submit="authenticate">
-                {{ $this->form }}
-
-                <x-filament-panels::form.actions
-                    :actions="$this->getCachedFormActions()"
-                    :full-width="$this->hasFullWidthFormActions()"
-                />
-            </x-filament-panels::form>
-            <div class="text-center pt-4">
-                <a class="text-primary-600"
-                   href="#!"
-                   onclick="event.preventDefault(); document.getElementById('logout-form').submit();">
-                    {{ __('filament-lockscreen::default.button.switch_account') }}
-                </a>
-                <form id="logout-form" action="{{ url(filament()->getLogoutUrl()) }}" method="POST" style="display: none;">
-                    {{ csrf_field() }}
-                </form>
-            </div>
-
-
-        </main>
     </x-filament-panels::page.simple>
+
+    <form id="logout-form" action="{{ url(filament()->getCurrentPanel()->getLogoutUrl()) }}" method="POST" style="display: none;">
+        {{ csrf_field() }}
+    </form>
+
 </div>
 
