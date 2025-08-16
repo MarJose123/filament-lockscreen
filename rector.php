f800a742434922da758c3c0160574c59d2b5db03
MarJose123 <18107626+MarJose123@users.noreply.github.com>
2025-08-16 23:30:59 +0800

    chore: Exclude config directory from Rector

--- rector.php
+++ rector.php
@@ -6,7 +6,6 @@ use Rector\Config\RectorConfig;
 
 return RectorConfig::configure()
     ->withPaths([
-        __DIR__.'/config',
         __DIR__.'/resources',
         __DIR__.'/routes',
         __DIR__.'/src',
