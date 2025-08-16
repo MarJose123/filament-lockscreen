15f4386ae42a1adeb9b9660b933ba6977c24b6e5
MarJose123 <18107626+MarJose123@users.noreply.github.com>
2025-08-16 23:27:47 +0800

    feat: Adopt Filament Schema API for lock screen

--- src/Http/Livewire/LockerScreen.php
+++ src/Http/Livewire/LockerScreen.php
@@ -6,19 +6,34 @@ use DanHarrin\LivewireRateLimiting\Exceptions\TooManyRequestsException;
 use DanHarrin\LivewireRateLimiting\WithRateLimiting;
 use Filament\Actions\Action;
 use Filament\Actions\ActionGroup;
+use Filament\AvatarProviders\UiAvatarsProvider;
 use Filament\Exceptions\NoDefaultPanelSetException;
 use Filament\Facades\Filament;
 use Filament\Forms\Components\TextInput;
+use Filament\Infolists\Components\ImageEntry;
+use Filament\Infolists\Components\TextEntry;
 use Filament\Notifications\Notification;
 use Filament\Pages\Concerns\InteractsWithFormActions;
 use Filament\Pages\SimplePage;
+use Filament\Schemas\Components\Actions;
+use Filament\Schemas\Components\Component;
+use Filament\Schemas\Components\EmbeddedSchema;
+use Filament\Schemas\Components\Form;
+use Filament\Schemas\Schema;
+use Filament\Support\Enums\Alignment;
 use Filament\Support\Enums\Width;
 use Illuminate\Contracts\Foundation\Application;
 use Illuminate\Contracts\Support\Htmlable;
+use Illuminate\Foundation\Auth\User;
 use Illuminate\Http\RedirectResponse;
 use Illuminate\Routing\Redirector;
+use Illuminate\Support\Facades\Blade;
+use Illuminate\Support\HtmlString;
 use lockscreen\FilamentLockscreen\Lockscreen;
 
+/**
+ * @property-read Schema $form
+ */
 class LockerScreen extends SimplePage
 {
     use InteractsWithFormActions;
@@ -28,67 +43,48 @@ class LockerScreen extends SimplePage
 
     protected static ?string $title = null;
 
-    protected string|Width|null $maxContentWidth = Width::Full;
+    protected string|Width|null $maxContentWidth = Width::Medium;
 
     protected ?string $heading = '';
 
     protected string $view = 'filament-lockscreen::page.auth.login';
 
+    /**
+     * @var array<string, mixed> | null
+     */
+    public ?array $data = [];
 
     /**
-     * @return \Illuminate\Foundation\Application|RedirectResponse|Redirector|object|void
-     *
      * @throws NoDefaultPanelSetException
      */
-    public function mount()
+    public function mount(): void
     {
-        /**
-         * Authentication Check
-         *
-         * Check if the request is still authenticated or not before rendering the page,
-         * if not authenticated, then redirect to the login page of the current panel, or default panel if the current panel could not be detected.
-         */
         if (! Filament::auth()->check()) {
             if (filament()->getCurrentPanel()) {
-                return redirect(filament()->getCurrentPanel()->getLoginUrl());
+                redirect()->intended(filament()->getCurrentPanel()->getLoginUrl());
             }
 
-            return redirect(filament()->getDefaultPanel()->getLoginUrl());
+            redirect()->intended(filament()->getDefaultPanel()->getLoginUrl());
         }
 
-        /**
-         * Redirect to the filament default home url if the session is not locked
-         */
         if (! session()->has('lockscreen')) {
             if (session()->has('url.intended')) {
-                return redirect()->intended();
+                redirect()->intended();
             }
 
-            return redirect(filament()->getDefaultPanel()->getPath());
+            redirect()->intended(filament()->getDefaultPanel()->getPath());
         }
+
     }
 
     public function authenticate(): Redirector|RedirectResponse|Application|null
     {
-        $data = $this->form->getState();
-        /*
-          *  Rate Limit
-          */
-        if (config('filament-lockscreen.rate_limit.enable_rate_limit', true)) {
+
+        if (Lockscreen::get()->isRateLimitEnabled()) {
             try {
                 $this->rateLimit(Lockscreen::get()->getRateLimitLimit());
             } catch (TooManyRequestsException $exception) {
-                Notification::make()
-                    ->title(__('filament-panels::pages/auth/login.notifications.throttled.title', [
-                        'seconds' => $exception->secondsUntilAvailable,
-                        'minutes' => ceil($exception->secondsUntilAvailable / 60),
-                    ]))
-                    ->body(array_key_exists('body', __('filament-panels::pages/auth/login.notifications.throttled') ?: []) ? __('filament-panels::pages/auth/login.notifications.throttled.body', [
-                        'seconds' => $exception->secondsUntilAvailable,
-                        'minutes' => ceil($exception->secondsUntilAvailable / 60),
-                    ]) : null)
-                    ->danger()
-                    ->send();
+                $this->getRateLimitedNotification($exception)->send();
 
                 if (Lockscreen::get()->isForceLogout()) {
                     $this->forceLogout();
@@ -101,43 +97,111 @@ class LockerScreen extends SimplePage
             }
         }
 
+        $this->form->getState();
+
         if (! Filament::auth()->attempt([
             Lockscreen::get()->getCustomTableColumns()[0] => Filament::auth()->user()->{Lockscreen::get()->getCustomTableColumns()[0]},
-            Lockscreen::get()->getCustomTableColumns()[1] => $data['password'],
+            Lockscreen::get()->getCustomTableColumns()[1] => $this->data['password'],
         ])) {
-            $this->addError('password', __('filament-panels::pages/auth/login.messages.failed'));
+            $this->addError('data.password', __('filament-panels::auth/pages/login.messages.failed'));
 
             return null;
         }
 
         // redirect to the main page and forge the lockscreen session
         session()->regenerate();
-        session()->forget('lockscreen');
-        session()->forget('session_last_activity');
+        $this->purgeSession();
 
         return redirect()->intended();
     }
 
+    protected function purgeSession(): void
+    {
+        session()->forget('lockscreen');
+        session()->forget('session_last_activity');
+    }
+
+    protected function getRateLimitedNotification(TooManyRequestsException $exception): ?Notification
+    {
+        return Notification::make()
+            ->title(__('filament-panels::auth/pages/login.notifications.throttled.title'))
+            ->body(array_key_exists('body', __('filament-panels::auth/pages/login.notifications.throttled') ?: []) ? __('filament-panels::auth/pages/login.notifications.throttled.body', [
+                'seconds' => $exception->secondsUntilAvailable,
+            ]) : null)
+            ->danger();
+    }
+
     protected function forceLogout(): void
     {
-        filament()->getCurrentPanel()->auth()->logout();
-
         Notification::make()
             ->title(__('filament-lockscreen::default.notification.title'))
             ->body(__('filament-lockscreen::default.notification.message'))
             ->danger()
             ->send();
+
+        filament()->getCurrentPanel()->auth()->logout();
     }
 
-    protected function getFormSchema(): array
+    public function form(Schema $schema): Schema
     {
-        return [
+        return $schema
+            ->components([
+                $this->getAvatarComponent(),
+                $this->getUserNameFormComponent(),
+                $this->getPasswordFormComponent(),
+            ]);
+
+    }
+
+    protected function getAvatarComponent(): Component
+    {
+        /** @var User $user */
+        $user = \filament()->getCurrentPanel()->auth()->user();
+
+        return ImageEntry::make('avatar')
+            ->hiddenLabel()
+            ->circular()
+            ->imageSize(80)
+            ->columnSpanFull()
+            ->defaultImageUrl((new UiAvatarsProvider)->get($user))
+            ->state(method_exists($user, 'getFilamentAvatarUrl') ? $user->getFilamentAvatarUrl() : (new UiAvatarsProvider)->get($user))
+            ->extraAttributes([
+                'style' => 'justify-content: center;',
+            ]);
+
+    }
+
+    protected function getUserNameFormComponent(): Component
+    {
+        return
+            TextEntry::make('username')
+                ->hiddenLabel()
+                ->id('username')
+                ->columnSpanFull()
+                ->extraAttributes([
+                    'style' => 'justify-content: center; display: flex;',
+                ])
+                ->state(Lockscreen::get()->isDisplayNameEnabled() ? filament()->getCurrentPanel()->auth()->user()->{Lockscreen::get()->displayName()} : '');
+    }
+
+    protected function getPasswordFormComponent(): Component
+    {
+        return
             TextInput::make('password')
+                ->name('password')
+                ->id('password')
                 ->label(__('filament-lockscreen::default.fields.password'))
                 ->password()
                 ->autocomplete(false)
-                ->required(),
-        ];
+                ->autofocus()
+                ->rule([
+                    'required',
+                ])
+                ->extraInputAttributes(['tabindex' => 1])
+                ->hint(
+                    new HtmlString(Blade::render(
+                        '<x-filament::link icon="heroicon-o-arrows-right-left" x-on:click="event.preventDefault(); document.getElementById(\'logout-form\').submit();" tabindex="3"  weight="normal" style="cursor: default; text-decoration:underline" color="gray"> {{ __(\'filament-lockscreen::default.button.switch_account\') }}</x-filament::link>')
+                    ));
     }
 
     public function getTitle(): Htmlable|string
@@ -160,6 +224,38 @@ class LockerScreen extends SimplePage
             ->submit('authenticate');
     }
 
+    public function content(Schema $schema): Schema
+    {
+        return $schema
+            ->components([
+                $this->getFormContentComponent(),
+            ])
+            ->statePath('data');
+    }
+
+    public function getFormContentComponent(): \Filament\Schemas\Components\Component
+    {
+        return Form::make([EmbeddedSchema::make('form')])
+            ->id('form')
+            ->livewireSubmitHandler('authenticate')
+            ->footer([
+                Actions::make($this->getFormActions())
+                    ->alignment(Alignment::End)
+                    ->fullWidth(),
+            ]);
+    }
+
+    public function defaultForm(Schema $schema): Schema
+    {
+        return $schema
+            ->statePath('data');
+    }
+
+    public function getHeading(): string|Htmlable
+    {
+        return __('filament-lockscreen::default.heading');
+    }
+
     /**
      * @return array<Action | ActionGroup>
      */
@@ -168,10 +264,5 @@ class LockerScreen extends SimplePage
         return [
             $this->getAuthenticateFormAction(),
         ];
-    }
-
-    protected function hasFullWidthFormActions(): bool
-    {
-        return true;
     }
 }
